942b2913c00f90aa98687c0e5a24b4d2d8532b95
Rafhael Hailar <rafhael4hailar@gmail.com>
2025-02-11 00:34:36 +0800

    feat: marketing navbar

--- app/marketing/page.tsx
+++ app/marketing/page.tsx
@@ -1,16 +1,27 @@
 import type { Route } from "./+types/page";
+import MarketingHeader from "./_components/marketing-header";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
+    { title: "ShopCo" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
 
 export default function Home() {
   return (
-    <div>
-      <div className="text-red-500">Hello Page!</div>
-    </div>
+    <main className="max-w-[1920px] w-full">
+      <div className="bg-black text-white text-center w-full">
+        <p className="text-sm font-light py-2">
+          Sign up and get 20% off to your first order.&nbsp;
+          <a href="#" className="underline underline-offset-4">
+            Sign Up Now
+          </a>
+        </p>
+      </div>
+      <div className="px-6 lg:px-24">
+        <MarketingHeader />
+      </div>
+    </main>
   );
 }
