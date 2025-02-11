d22bf1b25b395605e9f0bc914f28d11811ed78a0
Rafhael Hailar <rafhael4hailar@gmail.com>
2025-02-11 20:51:11 +0800

    feat: parterns logo.

--- app/marketing/_components/marketing-header.tsx
+++ app/marketing/_components/marketing-header.tsx
@@ -2,6 +2,11 @@ import { FiSearch } from "react-icons/fi";
 import { PiShoppingCartSimpleBold } from "react-icons/pi";
 import { LuCircleUserRound } from "react-icons/lu";
 import { RxHamburgerMenu } from "react-icons/rx";
+import VersaceLogo from "/partners/versace.svg";
+import ZaraLogo from "/partners/zara.svg";
+import GucciLogo from "/partners/gucci.svg";
+import PradaLogo from "/partners/prada.svg";
+import CalvinKleinLogo from "/partners/calvin-klein.svg";
 
 export default function MarketingHeader() {
   return (
@@ -65,8 +70,8 @@ export default function MarketingHeader() {
         </div>
       </nav>
       <div>
-        <div className="flex px-6 lg:px-24 gap-x-12 gap-y-2 lg:gap-y-12 flex-wrap xl:flex-nowrap">
-          <div className="xl:max-w-[800px] w-full flex flex-col gap-y-6 pt-8 lg:pt-18 pb-26">
+        <div className="flex xl:px-24 gap-x-12 flex-wrap xl:flex-nowrap">
+          <div className="px-6 lg:px-24 xl:px-0 xl:max-w-[800px] w-full flex flex-col gap-y-6 pt-8 lg:pt-18 pb-26">
             <h1 className="font-[IntegralCF] text-4xl lg:text-6xl lg:max-w-[600px]">
               FIND CLOTHES THAT MATCHES YOUR STYLE
             </h1>
@@ -76,12 +81,12 @@ export default function MarketingHeader() {
               of style.
             </p>
             <div>
-              <button className="min-w-full xl:min-w-0 bg-black text-white px-16 cursor-pointer py-3 rounded-4xl">
+              <button className="min-w-full md:min-w-0 bg-black text-white px-16 cursor-pointer py-3 rounded-4xl">
                 Shop Now
               </button>
             </div>
-            <div className="flex h-18 lg:mt-12 gap-x-5 lg:gap-x-26 gap-y-4 lg:gap-y-8 xl:gap-x-11 justify-center xl:justify-start flex-wrap xl:flex-nowrap">
-              <div className="flex items-center">
+            <div className="flex pl-3 md:pl-0 h-18 lg:mt-12 gap-x-4 sm:gap-x-14  md:gap-x-15 lg:gap-x-24 gap-y-4 lg:gap-y-8 xl:gap-x-11 justify-center xl:justify-start flex-wrap xl:flex-nowrap">
+              <div className="flex items-center justify-center">
                 <div>
                   <h3 className="text-2xl lg:text-4xl font-bold">200+</h3>
                   <p className="text-sm lg:text-md text-gray-500">
@@ -100,7 +105,7 @@ export default function MarketingHeader() {
                   </p>
                 </div>
               </div>
-              <div className="max-w-0.5 w-full hidden xl:block">
+              <div className="max-w-0.5 w-full sm:block hidden">
                 <div className="h-full bg-gray-200 w-full"></div>
               </div>
               <div className="flex items-center">
@@ -118,7 +123,23 @@ export default function MarketingHeader() {
           </div>
         </div>
       </div>
-      <div className="bg-black text-white px-6 lg:px-24"></div>
+      <div className="bg-black text-white px-6 lg:px-24 flex lg:justify-between py-8 flex-wrap gap-y-10 gap-x-5 lg:gap-x-0 justify-center">
+        <div className="flex items-center">
+          <img src={VersaceLogo} className="lg:w-[200px] w-[100px]" />
+        </div>
+        <div className="flex items-center">
+          <img src={ZaraLogo} className="lg:w-[110px] w-[55px]" />
+        </div>
+        <div className="flex items-center">
+          <img src={GucciLogo} className="lg:w-[210px] w-[110px]" />
+        </div>
+        <div className="flex items-center">
+          <img src={PradaLogo} className="lg:w-[220px] w-[110px]" />
+        </div>
+        <div className="flex items-center">
+          <img src={CalvinKleinLogo} className="lg:w-[220px] w-[120px]" />
+        </div>
+      </div>
     </section>
   );
 }
