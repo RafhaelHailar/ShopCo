786df172849599a08c5220ca289a62cd845206ab
Rafhael Hailar <rafhael4hailar@gmail.com>
2025-02-13 17:19:34 +0800

    style: responsive product category showcase.

--- app/marketing/_components/marketing-products-overview.tsx
+++ app/marketing/_components/marketing-products-overview.tsx
@@ -150,41 +150,46 @@ export default function MarketingProductsOverview() {
         </div>
         <ProductCategory category="top selling" items={newArrivalsData} />
       </div>
-      <div className="px-16 bg-zinc-100 py-18">
-        <div className="mb-16">
-          <h2 className="text-center text-5xl">BROWSE BY DRESS STYLE</h2>
+      <div className="px-6 xl:px-16 bg-zinc-100 py-12 xl:py-18">
+        <div className="flex justify-center mb-8 xl:mb-16">
+          <h2 className="text-3xl w-[250px] lg:w-auto text-center lg:text-5xl">
+            BROWSE BY DRESS STYLE
+          </h2>
         </div>
-        <div className="grid grid-cols-5 gap-4 h-[600px]">
-          <div className="col-span-2 rounded-2xl relative overflow-hidden">
+        <div className="xl:grid xl:grid-cols-5 flex-col flex gap-4 xl:h-[600px]">
+          <div className="w-full xl:h-auto h-[200px] col-span-2 rounded-2xl relative overflow-hidden">
             <img
               src={CasualStyleImg}
               className="absolute top-10 -left-5 scale-200"
             />
-            <p className="text-black absolute top-10 left-10 text-4xl font-bold">
+            <p className="text-black absolute top-5 left-8 xl:top-10 xl:left-10 text-2xl xl:text-4xl font-bold">
               Casual
             </p>
           </div>
-          <div className="col-span-3 rounded-2xl relative overflow-hidden">
+          <div className="w-full xl:h-auto h-[200px] bg-white col-span-3 rounded-2xl relative overflow-hidden">
             <img
               src={FormalStyleImg}
-              className="absolute top-20 left-80 scale-200"
+              className="absolute top-15 xl:top-20 left-40 xl:left-80 scale-220 xl:scale-200"
             />
-            <p className="text-black absolute top-10 left-10 text-4xl font-bold">
+            <p className="text-black absolute top-5 left-8 xl:top-10 xl:left-10 text-2xl xl:text-4xl font-bold">
               Formal
             </p>
           </div>
-          <div className="col-span-3 bg-white rounded-2xl relative overflow-hidden">
+          <div className="w-full xl:h-auto h-[200px] col-span-3 bg-white rounded-2xl relative overflow-hidden">
             <img
               src={PartyStyleImg}
-              className="absolute scale-110 -top-35 left-25"
+              className="absolute scale-110 -top-10 left-15 lg:-top-35 xl:left-25"
             />
-            <p className="text-black absolute top-10 left-10 text-4xl font-bold">
+            <p className="text-black absolute top-10 left-10 text-2xl xl:text-4xl font-bold">
               Party
             </p>
           </div>
-          <div className="col-span-2 bg-white rounded-2xl relative overflow-hidden">
-            <img src={GymStyleImg} className="absolute -top-35 left-27" />
-            <p className="text-black absolute top-10 left-10 text-4xl font-bold">
+          <div className="w-full xl:h-auto h-[200px] col-span-2 bg-white rounded-2xl relative overflow-hidden">
+            <img
+              src={GymStyleImg}
+              className="absolute -top-25 lg:-top-35 left-15 xl:left-27"
+            />
+            <p className="text-black absolute top-10 left-10 text-2xl xl:text-4xl font-bold">
               Gym
             </p>
           </div>
