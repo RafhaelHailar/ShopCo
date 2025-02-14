26f6c994182df2fea492c0abced322006a0276a5
Rafhael Hailar <rafhael4hailar@gmail.com>
2025-02-14 17:49:48 +0800

    style: responsive customer review.

--- app/marketing/_components/marketing-review.tsx
+++ app/marketing/_components/marketing-review.tsx
@@ -85,12 +85,14 @@ function CommentBox({
   totalComments,
 }: CommentBoxProps) {
   const swiper = useSwiper();
+  const peripheralBlurred = swiper.width >= 768;
   const leftPeripheralIndex = swiper.realIndex;
   const rightPeripheralIndex =
     (swiper.realIndex + totalVisibleSlides) % totalComments;
   return (
     <>
-      {index === leftPeripheralIndex || index === rightPeripheralIndex ? (
+      {peripheralBlurred &&
+      (index === leftPeripheralIndex || index === rightPeripheralIndex) ? (
         <div className="border border-gray-300 rounded-xl flex flex-col gap-y-2 px-10 py-8 blur-xs">
           <div className="flex gap-x-1">
             {new Array(5).fill(null).map((_, i) => (
@@ -128,8 +130,9 @@ function ReviewSlideCtrlButtons() {
   const slideTo = () => {
     swiper.slideTo(targetIndex);
   };
+
   return (
-    <div className="absolute -top-22 left-420 text-4xl flex gap-x-5">
+    <div className="absolute -top-12 lg:-top-12 xl:-top-24 right-0 xl:left-340 2xl:left-420 text-2xl xl:text-4xl flex gap-x-5">
       <button className="cursor-pointer" onClick={() => swiper.slidePrev()}>
         <GoArrowLeft />
       </button>
@@ -143,15 +146,15 @@ function ReviewSlideCtrlButtons() {
 export default function MarketingReview() {
   const [sWW, setSWW] = useState(0);
   return (
-    <section className="relative px-24 overflow-hidden py-24">
-      <div className="mb-14">
-        <h2 className="text-5xl">OUR HAPPY CUSTOMERS</h2>
+    <section className="relative px-6 xl:px-24 overflow-hidden py-12 xl:py-24">
+      <div className="mb-6 xl:mb-14 w-48 md:w-auto">
+        <h2 className="text-xl xl:text-5xl">OUR HAPPY CUSTOMERS</h2>
       </div>
       <Swiper
-        className="relative -left-110 w-[200%]"
+        className="relative xl:-left-90 2xl:-left-110 w-full xl:w-[200%]"
         loop={true}
-        spaceBetween={22}
-        slidesPerView={6}
+        spaceBetween={10}
+        slidesPerView={1}
         onSlideChange={() => {
           setSWW(Math.random());
         }}
@@ -162,6 +165,20 @@ export default function MarketingReview() {
           if (swiperContainer) {
             swiperContainer.style.overflow = "visible";
           }
+        }}
+        breakpoints={{
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 22,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 22,
+          },
+          1280: {
+            slidesPerView: 6,
+            spaceBetween: 22,
+          },
         }}
       >
         <ReviewSlideCtrlButtons />
