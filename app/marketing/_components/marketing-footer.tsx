146e53b00034cd3851d2096a173c4881f19c24b6
Rafhael Hailar <rafhael4hailar@gmail.com>
2025-02-15 02:41:23 +0800

    style: marketing footer responsive.

--- app/marketing/_components/marketing-footer.tsx
+++ app/marketing/_components/marketing-footer.tsx
@@ -11,19 +11,19 @@ import GooglePayLogo from "/logo/google-pay.svg";
 export default function MarketingFooter() {
   return (
     <footer>
-      <div className="px-24">
-        <div className="bg-black px-16 flex gap-x-12 py-12 rounded-3xl -mb-25 items-center z-10 relative">
+      <div className="px-6 xl:px-24">
+        <div className="bg-black px-6 xl:px-16 gap-y-8 flex flex-col xl:flex-row gap-x-12 py-8 xl:py-12 rounded-3xl -mb-25 items-center z-10 relative">
           <div>
-            <h2 className="text-white text-5xl">
+            <h2 className="text-white text-3xl xl:text-5xl">
               STAY UPTO DATE ABOUT OUR LATEST OFFERS
             </h2>
           </div>
-          <div className="w-2/4 flex flex-col gap-y-3">
-            <div className="bg-white flex px-5 py-2 rounded-4xl gap-x-2">
+          <div className="w-full xl:w-2/4 flex flex-col gap-y-3">
+            <div className="bg-white flex px-5 py-2 rounded-4xl gap-x-4">
               <TfiEmail className="text-gray-200 text-2xl" />
               <input
                 type="text"
-                className="font-bold w-full outline-none"
+                className="w-full outline-none"
                 placeholder="Enter your email address"
               />
             </div>
@@ -33,10 +33,10 @@ export default function MarketingFooter() {
           </div>
         </div>
       </div>
-      <div className="bg-gray-100 relative px-24 pt-38 pb-24">
-        <div className="grid grid-cols-5 gap-y-7">
-          <div className="flex flex-col gap-y-7">
-            <h2 className="text-4xl">SHOP.CO</h2>
+      <div className="bg-gray-100 relative px-6 xl:px-24 pt-38 pb-24">
+        <div className="grid grid-cols-5 gap-y-5 xl:gap-y-7">
+          <div className="flex flex-col gap-y-5 xl:gap-y-7 col-span-5 xl:col-span-1">
+            <h2 className="text-4xl -mb-2 xl:-mb-0">SHOP.CO</h2>
             <p className="text-md text-gray-500">
               We have clothes that suits your style and which you’re proud to
               wear. From women to men.
@@ -72,9 +72,9 @@ export default function MarketingFooter() {
               </li>
             </ul>
           </div>
-          <div className="flex justify-end">
-            <div className="flex flex-col gap-y-9">
-              <h5 className="text-xl font-semibold">COMPANY</h5>
+          <div className="flex justify-start xl:justify-end xl:col-span-1 col-span-2">
+            <div className="flex flex-col gap-y-4 xl:gap-y-9">
+              <h5 className="text-lg xl:text-xl font-semibold">COMPANY</h5>
               <ul className="flex flex-col gap-y-4 text-gray-600">
                 <li>
                   <a href="#">About</a>
@@ -91,9 +91,9 @@ export default function MarketingFooter() {
               </ul>
             </div>
           </div>
-          <div className="flex justify-end">
-            <div className="flex flex-col gap-y-9">
-              <h5 className="text-xl font-semibold">HELP</h5>
+          <div className="flex justify-center xl:justify-end xl:col-span-1 col-span-3">
+            <div className="flex flex-col gap-y-4 xl:gap-y-9">
+              <h5 className="text-lg xl:text-xl font-semibold">HELP</h5>
               <ul className="flex flex-col gap-y-4 text-gray-600">
                 <li>
                   <a href="#">Customer Support</a>
@@ -110,9 +110,9 @@ export default function MarketingFooter() {
               </ul>
             </div>
           </div>
-          <div className="flex justify-end">
-            <div className="flex flex-col gap-y-9">
-              <h5 className="text-xl font-semibold">FAQ</h5>
+          <div className="flex justify-start xl:justify-end xl:col-span-1 col-span-2">
+            <div className="flex flex-col gap-y-4 xl:gap-y-9">
+              <h5 className="text-lg xl:text-xl font-semibold">FAQ</h5>
               <ul className="flex flex-col gap-y-4 text-gray-600">
                 <li>
                   <a href="#">Account</a>
@@ -129,9 +129,9 @@ export default function MarketingFooter() {
               </ul>
             </div>
           </div>
-          <div className="flex justify-end">
-            <div className="flex flex-col gap-y-9">
-              <h5 className="text-xl font-semibold">RESOURCES</h5>
+          <div className="flex justify-center xl:justify-end xl:col-span-1 col-span-3">
+            <div className="flex flex-col gap-y-4 xl:gap-y-9">
+              <h5 className="text-lg xl:text-xl font-semibold">RESOURCES</h5>
               <ul className="flex flex-col gap-y-4 text-gray-600">
                 <li>
                   <a href="#">Free eBooks</a>
@@ -148,24 +148,24 @@ export default function MarketingFooter() {
               </ul>
             </div>
           </div>
-          <div className="h-0.5 bg-gray-200 col-span-5 mt-15"></div>
-          <div className="text-sm text-gray-700 col-span-4">
+          <div className="h-0.5 bg-gray-200 col-span-5 mt-4 xl:mt-15"></div>
+          <div className="text-sm text-center xl:text-left text-gray-700 col-span-5 xl:col-span-4">
             <p>Shop.co &copy; 2000-2025. All Rights Reserved</p>
           </div>
-          <ul className="flex gap-x-3">
-            <li className="bg-white flex justify-center items-center w-15 h-8 rounded-md border border-gray-200">
+          <ul className="flex justify-center xl:justify-start gap-x-3 col-span-5 xl:col-span-1">
+            <li className="bg-white flex justify-center items-center w-12 xl:w-15 h-8 rounded-md border border-gray-200">
               <img src={VisaLogo} />
             </li>
-            <li className="bg-white flex justify-center items-center w-15 h-8 rounded-md border-gray-200">
+            <li className="bg-white flex justify-center items-center w-12 xl:w-15 h-8 rounded-md border-gray-200">
               <img src={MasterCardLogo} />
             </li>
-            <li className="bg-white flex justify-center items-center w-15 h-8 rounded-md border-gray-200">
+            <li className="bg-white flex justify-center items-center w-12 xl:w-15 h-8 rounded-md border-gray-200">
               <img src={PayPalLogo} />
             </li>
-            <li className="bg-white flex justify-center items-center w-15 h-8 rounded-md border-gray-200">
+            <li className="bg-white flex justify-center items-center w-12 xl:w-15 h-8 rounded-md border-gray-200">
               <img src={ApplePayLogo} className="mt-2" />
             </li>
-            <li className="bg-white flex justify-center items-center w-15 h-8 rounded-md border-gray-200">
+            <li className="bg-white flex justify-center items-center w-12 xl:w-15 h-8 rounded-md border-gray-200">
               <img src={GooglePayLogo} />
             </li>
           </ul>
