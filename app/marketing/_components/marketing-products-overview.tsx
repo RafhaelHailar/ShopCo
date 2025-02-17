101acb528f6c37f207bf439430a154e11caebf36
Rafhael Hailar <rafhael4hailar@gmail.com>
2025-02-17 09:11:48 +0800

    chore: make product component available to other page.

--- app/marketing/_components/marketing-products-overview.tsx
+++ app/marketing/_components/marketing-products-overview.tsx
@@ -1,14 +1,7 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { FaStar, FaStarHalf } from "react-icons/fa";
-
-interface Product {
-  id: number;
-  image: string;
-  name: string;
-  rating: number;
-  price: number;
-  discount: number;
-}
+import type { Product } from "types/product";
+import ProductContainer from "components/product";
 
 interface ProductCategoryProps {
   category: string;
@@ -40,53 +33,9 @@ function ProductCategory({ category, items }: ProductCategoryProps) {
         onSwiper={(swiper) => console.log(swiper)}
       >
         {items.map((item: Product, i) => {
-          // rating will be a value from 0 to 100,
-          // that's why we have to convert it to a value from 0-5
-          const rating = (item.rating / 100) * 5;
-          const fullStarTotal = Math.floor(rating);
-          const haveHalfStar = fullStarTotal % rating >= 0.5;
-
-          const price = item.price - item.price * item.discount;
-
           return (
             <SwiperSlide key={item.id}>
-              <div className="w-full relative flex items-center justify-center">
-                <img src={item.image} className="w-full" />
-              </div>
-              <div className="flex flex-col gap-y-1 mt-4">
-                <h5 className="font-bold text-sm lg:text-lg">{item.name}</h5>
-                <div className="flex items-center gap-x-1">
-                  <div className="flex items-center">
-                    {new Array(fullStarTotal).fill(null).map((_, i) => (
-                      <FaStar key={i} className="text-amber-400" />
-                    ))}
-                    {haveHalfStar ? (
-                      <FaStarHalf className="text-amber-400" />
-                    ) : (
-                      <></>
-                    )}
-                  </div>
-                  <p className="text-xs mt-1">
-                    {rating}
-                    <i className="not-italic text-gray-500">/5</i>
-                  </p>
-                </div>
-                <div className="flex gap-x-2">
-                  <p className="font-bold text-lg">${price}</p>
-                  {item.discount > 0 ? (
-                    <>
-                      <p className="line-through text-gray-400 font-bold text-lg">
-                        ${item.price}
-                      </p>
-                      <p className="bg-rose-100 text-rose-500 font-medium text-xs flex justify-center items-center rounded-2xl px-3">
-                        -{item.discount * 100}%
-                      </p>
-                    </>
-                  ) : (
-                    <></>
-                  )}
-                </div>
-              </div>
+              <ProductContainer product={item} />
             </SwiperSlide>
           );
         })}
---
+++ components/product.tsx
@@ -0,0 +1,49 @@
+import { FaStar, FaStarHalf } from "react-icons/fa6";
+import type { Product } from "types/product";
+
+export default function ProductContainer({ product }: { product: Product }) {
+  // rating will be a value from 0 to 100,
+  // that's why we have to convert it to a value from 0-5
+  const rating = (product.rating / 100) * 5;
+  const fullStarTotal = Math.floor(rating);
+  const haveHalfStar = fullStarTotal % rating >= 0.5;
+
+  const price = product.price - product.price * product.discount;
+  return (
+    <div>
+      <div className="w-full relative flex items-center justify-center rounded-xl overflow-hidden">
+        <img src={product.image} className="w-full" />
+      </div>
+      <div className="flex flex-col gap-y-1 mt-4">
+        <h5 className="font-bold text-xs lg:text-lg">{product.name}</h5>
+        <div className="flex items-center gap-x-1">
+          <div className="flex items-center text-xs lg:text-md gap-x-0.5">
+            {new Array(fullStarTotal).fill(null).map((_, i) => (
+              <FaStar key={i} className="text-amber-400" />
+            ))}
+            {haveHalfStar ? <FaStarHalf className="text-amber-400" /> : <></>}
+          </div>
+          <p className="text-xs mt-1">
+            {rating}
+            <i className="not-italic text-gray-500">/5</i>
+          </p>
+        </div>
+        <div className="flex gap-x-2">
+          <p className="font-bold text-sm lg:text-lg">${price}</p>
+          {product.discount > 0 ? (
+            <>
+              <p className="line-through text-gray-400 font-bold text-sm lg:text-lg">
+                ${product.price}
+              </p>
+              <p className="bg-rose-100 text-rose-500 font-medium text-xs flex justify-center items-center rounded-2xl py-0.5 lg:py-0 px-1 lg:px-3">
+                -{product.discount * 100}%
+              </p>
+            </>
+          ) : (
+            <></>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
