c371cceb0f3471916a8a694e1d0de7251a11df45
Rafhael Hailar <rafhael4hailar@gmail.com>
2025-02-22 16:22:12 +0800

    chore: make displays customizable.

--- components/product.tsx
+++ components/product.tsx
@@ -2,26 +2,62 @@ import { FaStar, FaStarHalf } from "react-icons/fa6";
 import { Link } from "react-router";
 import type { Product } from "types/product";
 
+interface PriceDisplayOption {
+  priceTextSize?: string;
+  discountPX?: number;
+  discountPY?: number;
+  discountTextSize?: string;
+}
+
 export function PriceDisplay({
   price,
   discount,
+  option = {
+    priceTextSize: "xl",
+    discountPY: 1,
+    discountPX: 3,
+    discountTextSize: "xs",
+  },
 }: {
   price: number;
   discount: number;
+  option?: PriceDisplayOption;
 }) {
   const newPrice = price - price * discount;
 
   return (
     <div className="flex gap-x-2">
-      <p className="font-bold text-sm lg:text-xl">${newPrice}</p>
+      <p
+        className="font-bold text-sm lg:text-xl"
+        style={{
+          fontSize: `var(--text-${option.priceTextSize})`,
+        }}
+      >
+        ${newPrice}
+      </p>
       {discount > 0 ? (
         <>
-          <p className="line-through text-gray-400 font-bold text-sm lg:text-lg">
+          <p
+            className="line-through text-gray-400 font-bold text-sm"
+            style={{
+              fontSize: `var(--text-${option.priceTextSize})`,
+            }}
+          >
             ${price}
           </p>
-          <p className="bg-rose-100 text-rose-500 font-medium text-xs flex justify-center items-center rounded-2xl py-0.5 lg:py-0 px-1 lg:px-3">
-            -{discount * 100}%
-          </p>
+          <div className="flex items-center">
+            <p
+              className="bg-rose-100 text-rose-500 font-semibold text-lg flex justify-center items-center rounded-2xl py-1 px-1 lg:px-3"
+              style={{
+                fontSize: `var(--text-${option.discountTextSize})`,
+                lineHeight: `var(--tw-leading, var(--text-${option.discountTextSize}--line-height)`,
+                paddingBlock: `calc(var(--spacing) * ${option.discountPY})`,
+                paddingInline: `calc(var(--spacing) * ${option.discountPX})`,
+              }}
+            >
+              -{discount * 100}%
+            </p>
+          </div>
         </>
       ) : (
         <></>
@@ -30,7 +66,18 @@ export function PriceDisplay({
   );
 }
 
-export function StarDisplay({ rating }: { rating: number }) {
+interface StarDisplayOption {
+  starSpacing?: number;
+  letterStarSpacing?: number;
+}
+
+export function StarDisplay({
+  rating,
+  option = { letterStarSpacing: 1, starSpacing: 0.5 },
+}: {
+  rating: number;
+  option?: StarDisplayOption;
+}) {
   // rating will be a value from 0 to 100,
   // that's why we have to convert it to a value from 0-5
   const convertedRating = (rating / 100) * 5;
@@ -38,8 +85,18 @@ export function StarDisplay({ rating }: { rating: number }) {
   const haveHalfStar = fullStarTotal % convertedRating >= 0.5;
 
   return (
-    <div className="flex items-center gap-x-1">
-      <div className="flex items-center text-xs lg:text-xl gap-x-0.5">
+    <div
+      className="flex items-center"
+      style={{
+        columnGap: `calc(var(--spacing) * ${option.letterStarSpacing})`,
+      }}
+    >
+      <div
+        className="flex items-center text-xs lg:text-xl gap-x-0.5"
+        style={{
+          columnGap: `calc(var(--spacing) * ${option.starSpacing})`,
+        }}
+      >
         {new Array(fullStarTotal).fill(null).map((_, i) => (
           <FaStar key={i} className="text-amber-400" />
         ))}
